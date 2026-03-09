5324e3df47cdaaf934d8a41b0528b79b99e42188
lchabardes <louis@chabardes.net>
2026-03-08 19:58:39 -0700

    Adding tick and dt management

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
     while True:
@@ -17,6 +19,8 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick() / 1000
+        #print(f"deltaTime: {dt}")
 
 if __name__ == "__main__":
     main()
