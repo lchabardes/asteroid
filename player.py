ba7f8e77394a1899014ecbcb354572ab37d0c307
lchabardes <louis@chabardes.net>
2026-03-09 21:04:00 -0700

    Implementing rotation, not working yet

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, LINE_WIDTH
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED
 
 
 class Player(CircleShape):
@@ -21,4 +21,17 @@ class Player(CircleShape):
     def draw(self, screen): 
         triangle_coord = self.triangle()
         pygame.draw.polygon(screen, "white", triangle_coord, LINE_WIDTH)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+            print("Pressed a")
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+            print("Pressed d")
         
