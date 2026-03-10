c7da30ad71c97bd8c14264a39e5164f5da71cdb1
lchabardes <louis@chabardes.net>
2026-03-09 20:34:18 -0700

    Drawing triangle is functional

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 
 import pygame
 from logger import log_state
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from player import Player
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS
 
 def main():
     print(f"Starting Asteroids with pygame: {pygame.version.ver}")
@@ -10,14 +11,19 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x, y)
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
         dt = clock.tick(60) / 1000
         log_state()
         #print(f"deltaTime: {dt}")
---
+++ player.py
@@ -0,0 +1,24 @@
+import pygame
+from circleshape import CircleShape
+from constants import PLAYER_RADIUS, LINE_WIDTH
+
+
+class Player(CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
+        color = "white"
+
+    # in the Player class
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen): 
+        triangle_coord = self.triangle()
+        pygame.draw.polygon(screen, "white", triangle_coord, LINE_WIDTH)
+        
